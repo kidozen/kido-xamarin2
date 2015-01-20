8ff67128a37664dc16146d7a7d9d95abf99cebd3
ccarnero <christian.carnero@gmail.com>
2015-01-20 15:17:29 -0300

    fix packages references

--- sources/kido/common/FileExtensions.cs
+++ sources/kido/common/FileExtensions.cs
@@ -9,11 +9,14 @@ using System.Collections;
 using System.Collections.Generic;
 
 #if __ANDROID__
+using Android.Runtime;
 namespace Kidozen.Android
+    {
+    [Preserve(AllMembers = true)]
 #else
 namespace Kidozen.iOS
+    {
 #endif
-{
 	public static partial class FileExtensions
 	{
 		public static Task<MemoryStream> Download(this Files files, string path) {
